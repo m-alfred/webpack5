15040b25e951bd4e996b6ed362d52f55325874b4
miaokefu <269436241@qq.com>
2020-10-19 17:53:28 +0800

    feat: output contenthash

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -6,8 +6,8 @@ module.exports = {
     index: resolve('src/index.js')
   },
   output: {
-    filename: '[name].[hash:8].js',
-    chunkFilename: '[name].[hash:8].js',
+    filename: '[name].[contenthash:8].js',
+    chunkFilename: '[name].[contenthash:8].js',
     path: resolve('build'),
     // webpack-dev-server 也会默认从 publicPath 为基准，使用它来决定在哪个目录下启用服务，来访问 webpack 输出的文件。
     publicPath: '/'
