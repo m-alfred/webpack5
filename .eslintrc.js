24a5a37b79ec12f75d8ac45845476dea5c632502
miaokefu <269436241@qq.com>
2020-10-20 16:53:14 +0800

    feat: 引入eslint规范代码

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+const { resolve } = require('./config/utils');
+
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+    allowImportExportEverywhere: false,
+    codeFrame: true,
+  },
+  extends: [
+    // including ECMAScript 6+ and React.
+    require.resolve('eslint-config-airbnb'),
+  ],
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: {
+          resolve: {
+            alias: {
+              '@': resolve('src'),
+            },
+          },
+          extensions: ['.js', '.jsx'],
+        },
+      },
+    },
+  },
+  rules: {
+    'no-console': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/no-dynamic-require': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 'off',
+    'react/destructuring-assignment': 'off',
+  },
+  globals: {
+    window: true,
+    document: true,
+    XMLHttpRequest: true,
+    fetch: true,
+  },
+};
