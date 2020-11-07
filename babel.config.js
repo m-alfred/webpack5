bb62474d4a0c5c366384ae87af3adc4ea8631897
miaokefu <269436241@qq.com>
2020-11-07 13:50:48 +0800

    feat: 支持babe支持类属性\babel支持装饰器

--- babel.config.js
+++ babel.config.js
@@ -2,21 +2,25 @@ module.exports = (api) => {
   api.cache.using(() => process.env.NODE_ENV === 'development');
   const presets = [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
-        
+
         corejs: 3,
         targets: [
-          'android >= 4.4.4', 
-          'ios >= 8'
+          'android >= 4.4.4',
+          'ios >= 8',
         ],
         // 根据代码逻辑中用到的 ES6+语法进行方法的导入，而不是全部导入
-        useBuiltIns: "usage"
-      }
+        useBuiltIns: 'usage',
+      },
     ],
-    "@babel/preset-react"
-  ]
-  const plugins = ["react-hot-loader/babel"];
+    '@babel/preset-react',
+  ];
+  const plugins = [
+    'react-hot-loader/babel',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: false }],
+  ];
 
-  return { presets, plugins }
-}
+  return { presets, plugins };
+};
