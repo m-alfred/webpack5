087c9bfc6a3703888913d3a11a7c09c86e6ccc32
miaokefu <269436241@qq.com>
2020-12-10 18:49:47 +0800

    test: stylelint

--- src/containers/home/index.hook.js
+++ src/containers/home/index.hook.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import Title from '@/components/Title';
+import './index.less';
 
 function Example() {
   // 声明一个叫 "count" 的 state 变量
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className='the-home'>
       <Title text='hook component3352' />
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)} type='button'>
